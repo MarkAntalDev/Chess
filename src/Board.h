785a36f0a34bc286a91e84d7a805efc48edbbb6b
MarkAntalDev <76174563+MarkAntalDev@users.noreply.github.com>
2021-01-14 12:15:08 +0100

    first board

--- src/Board.h
+++ src/Board.h
@@ -3,12 +3,45 @@
 
 class ChessBoard{
 public:
-    ChessBoard(){
-
-    }
+    ChessBoard();
 
 private:
-    char board[120] = 
+    /***********************************************************************************************
+    ; egy 120 elemes 1 dimenziós tömb a gyorsabb elérés és könnyebb lépésgenerálás érdekében
+    ; 10*12 es táblát reprezentál amivel a pályáról való kilépés alacsony költséggel ellenőrizhető
+    ; 1 byte <-> char változó használata a kevesebb tárhely érdekében
+    ; 
+    ; Bábuk reprezentációja:
+    ;
+    ; 1 - fehér gyalog    | white pawn
+    ; 2 - fehér ló        | white knight
+    ; 3 - fehér futó      | white bishop
+    ; 4 - fehér bástya    | white rook
+    ; 5 - fehér királyanő | white queen
+    ; 6 - fehér király    | white king
+    ;
+    ; 1 - fekete gyalog    | black pawn
+    ; 2 - fekete ló        | black knight
+    ; 3 - fekete futó      | black bishop
+    ; 4 - fekete bástya    | black rook
+    ; 5 - fekete királyanő | black queen
+    ; 6 - fekete király    | black king
+    ;
+    ************************************************************************************************/
+    char board[120] = {
+      7,  7,  7,  7,  7,  7,  7,  7,  7,  7,
+      7,  7,  7,  7,  7,  7,  7,  7,  7,  7,
+      7,  4,  2,  3,  6,  5,  3,  2,  4,  7,
+      7,  1,  1,  1,  1,  1,  1,  1,  1,  7,
+      7,  0,  0,  0,  0,  0,  0,  0,  0,  7,
+      7,  0,  0,  0,  0,  0,  0,  0,  0,  7,
+      7,  0,  0,  0,  0,  0,  0,  0,  0,  7,
+      7,  0,  0,  0,  0,  0,  0,  0,  0,  7,
+      7, -1, -1, -1, -1, -1, -1, -1, -1,  7,
+      7, -4, -2, -3, -6, -5, -3, -2, -4,  7,
+      7,  7,  7,  7,  7,  7,  7,  7,  7,  7,
+      7,  7,  7,  7,  7,  7,  7,  7,  7,  7
+    };
 }
 
 #endif // _BOARD_H_
